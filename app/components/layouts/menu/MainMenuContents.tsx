f9899de8c18d8b3004c3a4e8942996086f9ce699
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-20 18:25:12 -0400

    Add basic main menu

---
+++ app/components/layouts/menu/MainMenuContents.tsx
@@ -0,0 +1,243 @@
+export default function MainMenuContents() {
+    return (
+        <div id="main-menu-contents-container">
+            <div id="main-menu-logo">
+                <img src="/images/icons/main_square.png" alt="PFC logo" />
+            </div>
+            <div id="main-menu-right-contents">
+                <ul className="nav">
+                    <li className="hover-trigger-menu-item">
+                        <a
+                            href="https://prisonfellowship.ca/about-us/"
+                            className="hover-trigger-menu-item"
+                        >
+                            About PFC
+                        </a>
+                        <ul className="sub-menu">
+                            <li id="menu-item-68">
+                                <a href="https://prisonfellowship.ca/about-us/who-we-are/">
+                                    Who We Are
+                                </a>
+                            </li>
+                            <li id="menu-item-35931">
+                                <a href="https://prisonfellowship.ca/facilitating-transformation/">
+                                    Facilitating Transformation
+                                </a>
+                            </li>
+                            <li id="menu-item-102">
+                                <a href="https://prisonfellowship.ca/about-us/pfc-team/">
+                                    PFC Team
+                                </a>
+                            </li>
+                            <li id="menu-item-105">
+                                <a href="https://prisonfellowship.ca/about-us/our-board/">
+                                    Our Board
+                                </a>
+                            </li>
+                            <li id="menu-item-850">
+                                <a href="https://prisonfellowship.ca/about-us/regional-offices/">
+                                    Regional Offices
+                                </a>
+                            </li>
+                            <li id="menu-item-98">
+                                <a href="https://prisonfellowship.ca/about-us/our-financials/">
+                                    Our Financials
+                                </a>
+                            </li>
+                            <li id="menu-item-851">
+                                <a href="https://prisonfellowship.ca/about-us/statement-of-faith/">
+                                    Statement of Faith
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-1847">
+                        <a
+                            href="https://prisonfellowship.ca/pfc-programs/"
+                            className="hover-trigger-menu-item"
+                        >
+                            Our Programs
+                        </a>
+                        <ul className="sub-menu">
+                            <li id="menu-item-830">
+                                <a href="https://prisonfellowship.ca/our-programs/prisoners/in-prison-programs/">
+                                    In Prison Programs
+                                </a>
+                                <ul className="sub-menu">
+                                    <li id="menu-item-75">
+                                        <a href="https://prisonfellowship.ca/our-programs/prisoners/in-prison-programs/faith-justice/">
+                                            Faith &amp; Justice
+                                        </a>
+                                    </li>
+                                    <li id="menu-item-76">
+                                        <a href="https://prisonfellowship.ca/our-programs/prisoners/in-prison-programs/pf-by-phone/">
+                                            PFC By Phone
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li id="menu-item-77">
+                                <a href="https://prisonfellowship.ca/our-programs/restorative-justice/">
+                                    Restorative Justice
+                                </a>
+                                <ul className="sub-menu">
+                                    <li id="menu-item-1143">
+                                        <a href="https://prisonfellowship.ca/our-programs/restorative-justice/sycamore-tree/">
+                                            The Sycamore Tree
+                                        </a>
+                                    </li>
+                                    <li id="menu-item-766">
+                                        <a href="https://prisonfellowship.ca/our-programs/restorative-justice/restorative-prison/">
+                                            Restorative Prison
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li id="menu-item-80">
+                                <a href="https://prisonfellowship.ca/our-programs/in-community-programs/">
+                                    In Community Programs
+                                </a>
+                                <ul className="sub-menu">
+                                    <li id="menu-item-79">
+                                        <a href="https://prisonfellowship.ca/our-programs/in-community-programs/bridgecare/">
+                                            Bridgecare
+                                        </a>
+                                        <ul className="sub-menu">
+                                            <li id="menu-item-1257">
+                                                <a href="https://prisonfellowship.ca/our-programs/in-community-programs/bridgecare/1life/">
+                                                    1Life
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-771">
+                                        <a href="https://prisonfellowship.ca/our-programs/in-community-programs/angel-tree/">
+                                            Angel Tree
+                                        </a>
+                                        <ul className="sub-menu">
+                                            <li id="menu-item-89">
+                                                <a href="https://prisonfellowship.ca/our-programs/in-community-programs/angel-tree/angel-tree-camping/">
+                                                    Angel Tree Camping
+                                                </a>
+                                            </li>
+                                            <li id="menu-item-90">
+                                                <a href="https://prisonfellowship.ca/our-programs/in-community-programs/angel-tree/angel-tree-christmas/">
+                                                    Angel Tree Christmas
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-62">
+                        <a
+                            href="https://prisonfellowship.ca/resources/"
+                            className="hover-trigger-menu-item"
+                        >
+                            Resources
+                        </a>
+                        <ul className="sub-menu">
+                            <li id="menu-item-64">
+                                <a href="https://prisonfellowship.ca/resources/books/">
+                                    Recommend Readings
+                                </a>
+                            </li>
+                            <li id="menu-item-1714">
+                                <a href="https://prisonfellowship.ca/resources/institutional-chaplains-partners/">
+                                    Institutional Chaplain Partners
+                                </a>
+                            </li>
+                            <li id="menu-item-100">
+                                <a href="https://prisonfellowship.ca/resources/churches-volunteers/church-resources/">
+                                    Church Resources
+                                </a>
+                            </li>
+                            <li id="menu-item-101">
+                                <a href="https://prisonfellowship.ca/resources/churches-volunteers/volunteer-resources/">
+                                    Volunteer Resources
+                                </a>
+                            </li>
+                            <li id="menu-item-772">
+                                <a
+                                    target="_blank"
+                                    href="https://prisonfellowshipbridgecare.ca/"
+                                >
+                                    Ex-Prisoner Resources
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul className="button-menu">
+                    <li>
+                        <a
+                            href="https://prisonfellowship.ca/get-involved/"
+                            className="hover-trigger-menu-item"
+                        >
+                            Get Involved
+                        </a>
+                        <ul className="sub-menu">
+                            <li id="menu-item-37352">
+                                <a href="https://prisonfellowship.ca/calendar/category/pfcevents/">
+                                    PFC Events
+                                </a>
+                            </li>
+                            <li id="menu-item-107">
+                                <a href="https://prisonfellowship.ca/get-involved/volunteer/">
+                                    Volunteer
+                                </a>
+                            </li>
+                            <li id="menu-item-108">
+                                <a href="https://prisonfellowship.ca/resources/institutional-chaplains/">
+                                    Institutional Chaplains
+                                </a>
+                            </li>
+                            <li id="menu-item-82">
+                                <a href="https://prisonfellowship.ca/get-involved/prayer-partners/">
+                                    Prayer Partners
+                                </a>
+                                <ul className="sub-menu">
+                                    <li id="menu-item-37358">
+                                        <a href="https://prisonfellowship.ca/calendar/category/prayer-calendar/">
+                                            Prayer Calendar
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li id="menu-item-83">
+                                <a href="https://prisonfellowship.ca/get-involved/raise-awareness/">
+                                    Raise awareness
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a
+                            href="https://prisonfellowship.ca/donate-online/"
+                            className="hover-trigger-menu-item"
+                        >
+                            Donate
+                        </a>
+                        <ul className="sub-menu">
+                            <li id="menu-item-1264">
+                                <a href="https://prisonfellowship.ca/donate-online/pathway-partners/">
+                                    Pathway Partners
+                                </a>
+                            </li>
+                            <li id="menu-item-398">
+                                <a href="https://prisonfellowship.ca/donate-online/ways-to-donate/">
+                                    Ways to Donate
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul>
+                    <li>Search</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
--- app/components/layouts/menu/MainTopMenu.tsx
+++ app/components/layouts/menu/MainTopMenu.tsx
@@ -1,9 +1,11 @@
+import MainMenuContents from "./MainMenuContents";
 import TopLinks from "./TopLinks";
 
 export default function MainTopMenu() {
     return (
         <>
             <TopLinks />
+            <MainMenuContents />
         </>
     );
 }
