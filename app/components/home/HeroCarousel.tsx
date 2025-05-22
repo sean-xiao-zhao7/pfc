6bfc55ffb6edb1b3b6cada4ea8f14f39ac1f1edc
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-22 15:20:29 -0400

    Add static hero area

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -1,11 +1,25 @@
+import MainButton from "../buttons/MainButton";
+
 export default function HeroCarousel() {
     return (
         <div id="hero-carousel-container">
-            <div className="singel-carousel-item-container">
+            <div className="single-carousel-item-container">
                 <img
                     src="/images/home/hero/item-1-kid.png"
                     alt="hero first image"
                 />
+                <div className="single-carousel-inner" id="carousel-angel-tree">
+                    <img
+                        src="/images/home/hero/angel-tree.png"
+                        alt="angel tree"
+                    />
+                    <h1>Give a Child an Adventure this Summer!</h1>
+                    <p>
+                        Help us send a prisoner’s child to an accredited
+                        Christian day or overnight camp!
+                    </p>
+                    <MainButton color="orange" text="READ MORE" />
+                </div>
             </div>
         </div>
     );
