32b85d15c4eff4c1b46d0345e35a530cdca4f4cc
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-22 16:10:24 -0400

    Add all section skeletons in home page

--- app/components/home/Home.tsx
+++ app/components/home/Home.tsx
@@ -5,37 +5,75 @@ export default function Home() {
     return (
         <main id="home">
             <HeroCarousel />
-            <div id="intro">
-                <div className="info">
-                    <img
-                        src="/images/home/cropped-favicon.png"
-                        alt="PFC logo"
-                    />
-                    <h1>Facilitating Transformation</h1>
-                    <p>
-                        Prison Fellowship Canada meets people where they are in
-                        their journey towards freedom. Whether a person is a
-                        prisoner, ex-prisoner, a child of an incarcerated
-                        parent, or the greater community including victims, we
-                        recognize that the effects of crime are far reaching. We
-                        also recognize that God’s unconditional love, grace and
-                        mercy reaches even further and to each and every one of
-                        us.
-                    </p>
-                    <p>
-                        We welcome you to join us in seeing lives transformed…
-                        those whose whom we serve… and our own…
-                    </p>
-                    <PfcButton color="black" text="LEARN MORE" />
+
+            {/* Facilitating Transformation */}
+            <div id="after-carousel-contents-container">
+                <div id="intro">
+                    <div className="info">
+                        <img
+                            src="/images/home/cropped-favicon.png"
+                            alt="PFC logo"
+                        />
+                        <h1>Facilitating Transformation</h1>
+                        <p>
+                            Prison Fellowship Canada meets people where they are
+                            in their journey towards freedom. Whether a person
+                            is a prisoner, ex-prisoner, a child of an
+                            incarcerated parent, or the greater community
+                            including victims, we recognize that the effects of
+                            crime are far reaching. We also recognize that God’s
+                            unconditional love, grace and mercy reaches even
+                            further and to each and every one of us.
+                        </p>
+                        <p>
+                            We welcome you to join us in seeing lives
+                            transformed… those whose whom we serve… and our own…
+                        </p>
+                        <PfcButton color="black" text="LEARN MORE" />
+                    </div>
+                    <iframe
+                        src="https://www.youtube.com/embed/CqjnuF1bU_Q"
+                        title="Prison Fellowship Canada - Pathway to Change"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    ></iframe>
                 </div>
-                <iframe
-                    src="https://www.youtube.com/embed/CqjnuF1bU_Q"
-                    title="Prison Fellowship Canada - Pathway to Change"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                ></iframe>
+
+                {/* Happening Now at PFC */}
+
+                <div id="happening">
+                    <h1>Happening Now at PFC</h1>
+                    <div>
+                        <div>
+                            <img
+                                src="/images/home/hero/item-1-kid.png"
+                                alt="angel tree camping"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {/* PROGRAMS */}
+
+                <div id="programs"></div>
+
+                {/* Testimony / RESTORATIVE JUSTICE */}
+
+                <div id="testimony"></div>
+
+                {/* SIGN UP TO STAY UP TO DATE */}
+
+                <div id="sign-up"></div>
+
+                {/* triple columns */}
+
+                <div id="triple-columns"></div>
+
+                {/* get involved */}
+
+                <div id="get-involved"></div>
             </div>
         </main>
     );
