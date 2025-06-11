6279027e2f16b55a00fed076fa90db8b68151ceb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-06-11 13:17:55 -0400

    Add our financials page

--- app/components/pages/OurFinancials.tsx
+++ app/components/pages/OurFinancials.tsx
@@ -17,9 +17,69 @@ export default function OurFinancials() {
                     the images to download a PDF copy and review what PFC has
                     been doing every day.
                 </p>
-                <p>
+            </div>
+            <div className="body center" id="main">
+                <h1>
                     Click on images to download PDF copies of our Annual Report
-                </p>
+                </h1>
+                <div className="center box-shadow">
+                    <img
+                        src="/images/pages/our_financials/Annual-Report-Cover-image-1.png"
+                        alt="PFC logo"
+                        className="pfc-logo"
+                    />
+                    <h2>2023 Report</h2>
+                </div>
+                <div className="two-cols">
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/2022-Annual-Report-EN-2048x1583.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2022 Report</h2>
+                    </div>
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/Annual-Report-2021.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2021 Report</h2>
+                    </div>
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/2020-Report-Cover-1.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2020 Report</h2>
+                    </div>
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/2019-Report-Cover-1.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2019 Report</h2>
+                    </div>
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/2018-Report-Cover-1.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2018 Report</h2>
+                    </div>
+                    <div className="center">
+                        <img
+                            src="/images/pages/our_financials/2017-Report-Cover-1.jpg"
+                            alt="PFC logo"
+                            className="pfc-logo"
+                        />
+                        <h2>2017 Report</h2>
+                    </div>
+                </div>
             </div>
         </div>
     );
