812382d81ba93163cfecf8afc71507d959cd2b20
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-06-11 13:32:46 -0400

    Complete statement of faith

---
+++ app/components/pages/StatementOfFaith.tsx
@@ -0,0 +1,72 @@
+export default function StatementOfFaith() {
+    return (
+        <div className="page-container" id="statement-of-faith">
+            <div className="slogan">
+                <img
+                    src="/images/home/cropped-favicon.png"
+                    alt="PFC logo"
+                    className="pfc-logo"
+                />
+                <h1>PFC Statement of Faith</h1>
+            </div>
+            <div className="body">
+                <h1>Nicene Creed</h1>
+                <p>
+                    We believe in one God, the Father, the Almighty, maker of
+                    heaven and earth, of all that is, seen and unseen.
+                    <br />
+                    We believe in one Lord, Jesus Christ, the only Son of God,
+                    eternally begotten of the Father, God from God, Light from
+                    Light, true God from true God, begotten, not made, of one
+                    Being with the Father; through Him, all things were made.
+                    <br />
+                    For us and for our salvation he came down from heaven, was
+                    incarnate of the Holy Spirit and the Virgin Mary and became
+                    truly human.
+                    <br />
+                    For our sake he was crucified under Pontius Pilate; he
+                    suffered death and was buried.
+                    <br />
+                    On the third day, he rose again in accordance with the
+                    Scriptures; he ascended into heaven and is seated at the
+                    right hand of the Father.
+                    <br />
+                    He will come again in glory to judge the living and the
+                    dead, and his kingdom will have no end.
+                    <br />
+                    We believe in the Holy Spirit, the Lord, the giver of life,
+                    who proceeds from the Father [and the Son], who with the
+                    Father and the Son is worshipped and glorified, who has
+                    spoken through the prophets.
+                    <br />
+                    We believe in one holy catholic and apostolic Church. We
+                    acknowledge one baptism for the forgiveness of sins.
+                    <br />
+                    We look for the resurrection of the dead, and the life of
+                    the world to come.
+                </p>
+            </div>
+            <div className="body">
+                <h1>Apostles Creed</h1>
+                <p>
+                    We believe in God, the Father Almighty, Creator of heaven
+                    and earth.
+                    <br />
+                    We believe in Jesus Christ, His only Son, our Lord, who was
+                    conceived by the Holy Spirit and born of the virgin Mary. He
+                    suffered under Pontius Pilate, was crucified, died, and was
+                    buried; he descended to hell. The third day he rose again
+                    from the dead. He ascended to heaven and is seated at the
+                    right hand of God the Father Almighty. From there He will
+                    come to judge the living and the dead.
+                    <br />
+                    We believe in the Holy Spirit, the holy catholic church, the
+                    communion of saints, the forgiveness of sins, the
+                    resurrection of the body, and the life everlasting.
+                    <br />
+                    Amen.
+                </p>
+            </div>
+        </div>
+    );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -14,5 +14,9 @@ export default [
     route("about-us/pfc-team", "./components/pages/PFCTeam.tsx"),
     route("about-us/our-board", "./components/pages/OurBoard.tsx"),
     route("about-us/our-financials", "./components/pages/OurFinancials.tsx"),
+    route(
+        "about-us/statement-of-faith",
+        "./components/pages/StatementOfFaith.tsx"
+    ),
     route("*", "./components/pages/NotFound.tsx"),
 ] satisfies RouteConfig;
