97ffd9f9f833f4ba18dda018d02d835d5b4739da
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-06-11 13:35:27 -0400

    Move all about PFC pages into its own child folder

--- app/routes.ts
+++ app/routes.ts
@@ -2,21 +2,24 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("./components/home/Home.tsx"),
-    route("about-us/who-we-are", "./components/pages/WhoWeAre.tsx"),
+    route("about-us/who-we-are", "./components/pages/about_pfc/WhoWeAre.tsx"),
     route(
         "about-us/regional-offices",
-        "./components/pages/RegionalOffices.tsx"
+        "./components/pages/about_pfc/RegionalOffices.tsx"
     ),
     route(
         "facilitating-transformation",
-        "./components/pages/FacilitatingTransformation.tsx"
+        "./components/pages/about_pfc/FacilitatingTransformation.tsx"
     ),
-    route("about-us/pfc-team", "./components/pages/PFCTeam.tsx"),
-    route("about-us/our-board", "./components/pages/OurBoard.tsx"),
-    route("about-us/our-financials", "./components/pages/OurFinancials.tsx"),
+    route("about-us/pfc-team", "./components/pages/about_pfc/PFCTeam.tsx"),
+    route("about-us/our-board", "./components/pages/about_pfc/OurBoard.tsx"),
+    route(
+        "about-us/our-financials",
+        "./components/pages/about_pfc/OurFinancials.tsx"
+    ),
     route(
         "about-us/statement-of-faith",
-        "./components/pages/StatementOfFaith.tsx"
+        "./components/pages/about_pfc/StatementOfFaith.tsx"
     ),
-    route("*", "./components/pages/NotFound.tsx"),
+    route("*", "./components/pages/about_pfc/NotFound.tsx"),
 ] satisfies RouteConfig;
