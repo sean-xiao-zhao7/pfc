4351304a03017dc978c804f694f87846f3b57f82
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-06-11 20:56:38 -0400

    Complete bridgecare

--- app/components/pages/our_programs/BridgeCare.tsx
+++ app/components/pages/our_programs/BridgeCare.tsx
@@ -75,6 +75,113 @@ export default function BridgeCare() {
                     <p>Luke 10: 33-34 (NIV)</p>
                 </div>
             </PfcBlock>
+            <div className="body">
+                <div className="two-cols">
+                    <div className="box">
+                        <img
+                            src="/images/pages/bridgecare/Annual-Report-2021-Images-File-1_Page_07-980x654.jpg"
+                            alt="prison phone call"
+                        />
+                        <div className="center">
+                            <h2>BridgeCare Phone Line</h2>
+                            <p>
+                                Bridgecare phone line is a toll-free phone line
+                                available to chaplains in the provincial systems
+                                across Canada. This phone line allows prisoners
+                                an opportunity to connect with PFC up to six
+                                months prior to leaving prison. Individuals get
+                                contacts for relational and material supports
+                                upon leaving the institution. This line is open
+                                to people of any faith or of no faith. Prisoners
+                                who express an interest in the Christian faith
+                                are referred to the Bridgecare program details.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img
+                            src="/images/pages/bridgecare/100102p_1Life.jpg"
+                            alt="1Life"
+                        />
+                        <div className="center">
+                            <h2>1Life Simulation</h2>
+                            <p>
+                                PFC invites participants from all walks of life
+                                to take part in an experiential game in order to
+                                to bring meaningful solutions to break the cycle
+                                of crime that too many people and communities
+                                are caught in.
+                            </p>
+                            <PfcButton
+                                color="black"
+                                text="Learn more"
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img
+                            src="/images/pages/bridgecare/bridgecare-screencapture-1.jpg"
+                            alt="Bridgecare screenshot"
+                        />
+                        <div className="center">
+                            <h2>BridgeCare DIRECTORY</h2>
+                            <p>
+                                With the click of a button, individuals can find
+                                their way to over 1800 organizations across
+                                Canada offering practical support as they
+                                integrate back into the community.
+                            </p>
+                            <PfcButton
+                                color="black"
+                                text="BridgeCare DIRECTORY"
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img
+                            src="/images/shared/church-res-cross.jpg"
+                            alt="cross"
+                        />
+                        <div className="center">
+                            <h2>Recommended Reading</h2>
+                            <p>
+                                Access an extensive database with books,
+                                journals, articles and spiritual encouragement.
+                            </p>
+                            <PfcButton
+                                color="black"
+                                text="Learn more"
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img
+                            src="/images/shared/people-talking-in-a-circle.jpg"
+                            alt="people talking in a circle"
+                        />
+                    </div>
+                    <div className="box">
+                        <div className="center">
+                            <h2>The Sycamore Tree</h2>
+                            <p>
+                                Our restorative justice program offers those who
+                                have caused harm to others through crime and
+                                those who have been harmed to gain a joint
+                                understanding of the impact of crime and proceed
+                                along the pathway to healing and transformation
+                            </p>
+                            <PfcButton
+                                color="black"
+                                text="Learn more"
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
             <SpaceDivider />
         </div>
     );
