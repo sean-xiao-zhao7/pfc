19db27a800d2ca57b1903b36f7451184d414745b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-07-15 16:50:27 -0400

    Complete facilitating transformation audit

--- app/components/pages/about_pfc/FacilitatingTransformation.tsx
+++ app/components/pages/about_pfc/FacilitatingTransformation.tsx
@@ -1,3 +1,6 @@
+import PfcBlock from "~/components/blocks/PfcBlock";
+import SpaceDivider from "~/components/blocks/SpaceDivider";
+
 export default function FacilitatingTransformation() {
     return (
         <div className="page-container" id="facilitating-transformation">
@@ -7,12 +10,7 @@ export default function FacilitatingTransformation() {
                 </h1>
             </div>
             <div className="body">
-                <div className="center">
-                    <img
-                        src="/images/home/cropped-favicon.png"
-                        alt="PFC logo"
-                        className="pfc-logo"
-                    />
+                <PfcBlock color="black">
                     <p>
                         “All this is from God, who reconciled us to himself
                         through Christ and gave us the ministry of
@@ -22,9 +20,11 @@ export default function FacilitatingTransformation() {
                         reconciliation. We are therefore Christ’s ambassadors,
                         as though God were making his appeal through us. We
                         implore you on Christ‘s behalf: Be reconciled to God.”
-                        <br />2 Corinthians 5:18
                     </p>
-                </div>
+                    <div className="center">
+                        <p>2 Corinthians 5:18</p>
+                    </div>
+                </PfcBlock>
                 <p>
                     Each one of us represents a complex story of how we’ve
                     become who we are today. Our stories include moments of joy,
@@ -33,6 +33,7 @@ export default function FacilitatingTransformation() {
                     not to be defined only by our worst or most painful moments
                     – whether self-inflicted or through others’ choices.
                 </p>
+                <SpaceDivider />
                 <div className="two-cols">
                     <div>
                         <p>
@@ -45,6 +46,7 @@ export default function FacilitatingTransformation() {
                             crimes often serve their sentences for life, even
                             when and long after their incarceration ends.
                         </p>
+                        <SpaceDivider />
                         <p>
                             It is clear that the journey to incarceration or
                             conviction is complex: generational trauma,
@@ -76,6 +78,7 @@ export default function FacilitatingTransformation() {
                             and we all have the potential for redemption,
                             transformation and growth.
                         </p>
+                        <SpaceDivider />
                         <p>
                             We know this is possible because we believe in a God
                             who made us all in His image – who provided hope for
@@ -86,13 +89,14 @@ export default function FacilitatingTransformation() {
                         </p>
                     </div>
                 </div>
-                <p className="cursive">
+                <SpaceDivider />
+            </div>
+            <PfcBlock color="blue">
+                <p className="emphasis">
                     We believe that full healing and restoration for crimes
                     committed is possible through the reconciliation to God, to
                     self, to family, to victims, and to community.
                 </p>
-            </div>
-            <div className="slogan">
                 <p>
                     Prison Fellowship Canada’s mandate is to meet people who
                     have committed crimes or who are impacted by crime –
@@ -107,33 +111,33 @@ export default function FacilitatingTransformation() {
                     back, to be part of a story of restoration and
                     transformation for them and all impacted by their decisions.
                 </p>
-            </div>
+            </PfcBlock>
+            <SpaceDivider />
             <div className="body">
                 <div className="two-cols">
                     <img
                         src="/images/pages/facilitating-transformation/facilitating-transformation3.png"
                         alt="facilitating transformation image 3"
                     />
-                    <div>
-                        <p>
-                            Our ultimate goal is to facilitate the
-                            transformation of lives – prisoners, families and
-                            victims – so that they all can experience spiritual
-                            freedom and have healthy relationships with God,
-                            others and themselves. This type of transformation
-                            is enabled by our partnerships with the government,
-                            Corrections staff, and the Canadian Christian
-                            community.
-                        </p>
-                    </div>
+                    <p>
+                        Our ultimate goal is to facilitate the transformation of
+                        lives – prisoners, families and victims – so that they
+                        all can experience spiritual freedom and have healthy
+                        relationships with God, others and themselves. This type
+                        of transformation is enabled by our partnerships with
+                        the government, Corrections staff, and the Canadian
+                        Christian community.
+                    </p>
                 </div>
-                <p className="center">
+                <SpaceDivider />
+                <p>
                     Those partnerships allow our staff and our volunteers to
                     engage with prison staff and those who are in prison using
                     our evidence-based and faith-based programming in over 135
                     correctional facilities across Canada.
                 </p>
-                <p className="center">
+                <SpaceDivider />
+                <p>
                     We support the transformation of prisoners seeking to be
                     reconciled and reintegrated into their families and
                     communities. These efforts are reinforced by mobilizing the
@@ -142,19 +146,25 @@ export default function FacilitatingTransformation() {
                     crime and families of prisoners.
                 </p>
             </div>
-            <div className="slogan">
-                <img
-                    src="/images/home/cropped-favicon.png"
-                    alt="PFC logo"
-                    className="pfc-logo"
-                />
-                <p>
-                    These combined efforts contribute to justice – meaning God
-                    takes what has fallen over and sets it up straight – meaning
-                    righted relationships– for all, where everyone is able to
-                    thrive according to God’s intended creation and sovereignty.
-                </p>
-            </div>
+            <SpaceDivider />
+            <PfcBlock color="orange">
+                <div className="center">
+                    <img
+                        src="/images/home/cropped-favicon.png"
+                        alt="PFC logo"
+                        className="pfc-logo"
+                    />
+                    <p>
+                        These combined efforts contribute to justice – meaning
+                        God takes what has fallen over and sets it up straight
+                    </p>
+                    <p>
+                        – meaning righted relationships– for all, where everyone
+                        is able to thrive according to God’s intended creation
+                        and sovereignty.
+                    </p>
+                </div>
+            </PfcBlock>
         </div>
     );
 }
